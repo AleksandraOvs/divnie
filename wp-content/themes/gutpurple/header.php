34d49480130f2be5fd1a07aa7e347f0bda97e009
aleksandra <aleksandra.ovsia@gmail.com>
2026-02-05 15:41:16 +0300

    eds

--- wp-content/themes/gutpurple/header.php
+++ wp-content/themes/gutpurple/header.php
@@ -22,6 +22,72 @@
 	<?php wp_head(); ?>
 
 	<script src="//code.jivo.ru/widget/06WBWEgOHa"></script>
+
+	<!-- Yandex.Metrika counter -->
+	<script type="text/javascript">
+		(function(m, e, t, r, i, k, a) {
+			m[i] = m[i] || function() {
+				(m[i].a = m[i].a || []).push(arguments)
+			};
+			m[i].l = 1 * new Date();
+			for (var j = 0; j < document.scripts.length; j++) {
+				if (document.scripts[j].src === r) {
+					return;
+				}
+			}
+			k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+		})(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym');
+
+		ym(94866918, 'init', {
+			webvisor: true,
+			clickmap: true,
+			referrer: document.referrer,
+			url: location.href,
+			accurateTrackBounce: true,
+			trackLinks: true
+		});
+	</script>
+	<noscript>
+		<div><img src="https://mc.yandex.ru/watch/94866918" style="position:absolute; left:-9999px;" alt="" /></div>
+	</noscript>
+	<!-- /Yandex.Metrika counter -->
+
+
+
+
+
+
+	<!-- Yandex.Metrika counter -->
+	<script type="text/javascript">
+		(function(m, e, t, r, i, k, a) {
+			m[i] = m[i] || function() {
+				(m[i].a = m[i].a || []).push(arguments)
+			};
+			m[i].l = 1 * new Date();
+			for (var j = 0; j < document.scripts.length; j++) {
+				if (document.scripts[j].src === r) {
+					return;
+				}
+			}
+			k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+		})(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym');
+
+		ym(97343746, 'init', {
+			webvisor: true,
+			clickmap: true,
+			ecommerce: "dataLayer",
+			referrer: document.referrer,
+			url: location.href,
+			accurateTrackBounce: true,
+			trackLinks: true
+		});
+	</script>
+	<noscript>
+		<div><img src="https://mc.yandex.ru/watch/97343746" style="position:absolute; left:-9999px;" alt="" /></div>
+	</noscript>
+	<!-- /Yandex.Metrika counter -->
+
+
 </head>
 
 <body <?php body_class(); ?>>
